618107131b5cfb05e83d7b2701882a94708b0ae9
srcecde <chiragr83@gmail.com>
2017-02-13 12:45:41 +0530

    Added seach by keyword Returns(Channel, Video, Playlist)

--- youtube_api_cmd.py
+++ youtube_api_cmd.py
@@ -17,8 +17,8 @@ from urllib.parse import urlparse, urlencode, parse_qs
 from urllib.request import  urlopen
 
 
-
 YOUTUBE_COMMENT_URL = 'https://www.googleapis.com/youtube/v3/commentThreads'
+YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 
 class YouTubeApi():
@@ -75,10 +75,62 @@ class YouTubeApi():
         except:
             print("Cannot Open URL or Fetch comments at a moment")
 
+    def search_keyword(self):
+        parser = argparse.ArgumentParser()
+        mxRes = 20
+        parser.add_argument("--search", help="Search Term", default="YouTube")
+        parser.add_argument("--max", help="number of results to return")
+        parser.add_argument("--key", help="Required API key")
+
+        args = parser.parse_args()
+
+        if not args.max:
+            args.max = mxRes
+
+        if not args.key:
+            exit("Please specify API key using the --key= parameter.")
+
+        parms = {
+                    'q': args.search,
+                    'part': 'id,snippet',
+                    'maxResults': args.max,
+                    'key': args.key
+                }
+        try:
+            f = urlopen(YOUTUBE_SEARCH_URL + '?' + urlencode(parms))
+            data = f.read()
+            f.close()
+            matches = data.decode("utf-8")
+
+            search_response = json.loads(matches)
+
+            videos = []
+            channels = []
+            playlists = []
+
+            for search_result in search_response.get("items", []):
+                if search_result["id"]["kind"] == "youtube#video":
+                  videos.append("{} ({})".format(search_result["snippet"]["title"],
+                                             search_result["id"]["videoId"]))
+                elif search_result["id"]["kind"] == "youtube#channel":
+                  channels.append("{} ({})".format(search_result["snippet"]["title"],
+                                               search_result["id"]["channelId"]))
+                elif search_result["id"]["kind"] == "youtube#playlist":
+                  playlists.append("{} ({})".format(search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+            print("Videos:\n".join(videos), "\n")
+            print("Channels:\n".join(channels), "\n")
+            print("Playlists:\n".join(playlists), "\n")
+
+        except:
+            print("Cannot Open URL or Fetch comments at a moment")
+
 
 def main():
     y = YouTubeApi()
-    y.get_video_comment()
+    # y.get_video_comment()
+    y.search_keyword()
 
 
 if __name__ == '__main__':
